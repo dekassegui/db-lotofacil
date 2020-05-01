5d54cbaa679e9ef69a148011e0c7e1a9e822ec4a
dekassegui <dekassegui@gmail.com>
2020-04-30 21:27:57 -0300

    Melhorias no R script das reincidências.

--- R/reincidencias.R
+++ R/reincidencias.R
@@ -1,50 +1,57 @@
 #!/usr/bin/Rscript --no-init-file
 
-library(RSQLite)
+library(RSQLite)  # r-cran-sqlite <-- Database Interface R driver for SQLite
 
 con <- dbConnect(SQLite(), 'loto.sqlite')
 
-# requisita criação de tabela temporária das bolas reincidentes nos concursos
-rs <- dbSendStatement(con, 'create temp table bolas_reincidentes as select a.concurso, a.bola from bolas_sorteadas as a join bolas_sorteadas as b on (a.concurso-1 == b.concurso and a.bola == b.bola)')
-dbClearResult(rs)
-
-# contagem dos concursos com mesmo número de bolas reincidentes
-dat <- dbGetQuery(con, 'select n, count(concurso) as m from ( select concurso, count(bola) as n from bolas_reincidentes group by concurso ) group by n')
+# data frame dos concursos e respectivas bolas reincidentes
+dat <- dbGetQuery(con, 'SELECT a.concurso, a.bola
+  FROM bolas_sorteadas AS a JOIN bolas_sorteadas AS b
+    ON (a.concurso-1 == b.concurso AND a.bola == b.bola)')
 
 dbDisconnect(con)
 
+possiveis <- c(5:15)  # quantidades possíveis de bolas reincidentes num concurso
+
+frequencias <- tabulate(
+    table(dat$concurso),  # frequências dos concursos distintos via contagem
+                          # das bolas reincidentes em cada concurso
+    nbins=15              # maior quantidade possível de bolas reincidentes
+  )[possiveis]            # seleciona frequências possíveis
+
+fname="img/reincidencias.png"
+
 png(
-  filename="img/reincidencias.png", width=600, height=600, pointsize=16,
-  family="Roboto Condensed"
+  filename=fname, width=600, height=600, pointsize=16, family="Roboto Condensed"
 )
 
 par(
-  mar=c(4.5, 4.5, 3.5, 2), las=1,
-  cex.main=1.5, col.main="navy",
+  mar=c(3.5, 3.7, 3, 1.5), mgp=c(2, .75, 0), las=1, fg="gray50",
+  cex.main=1.475, col.main="navy",
   cex.lab=1.25, font.lab=2, col.lab="slateblue"
 )
 
-upper=(max(dat$m)%/%50+1)*50
+major=(max(frequencias)%/%50+1)*50  # maior valor representável no eixo x
 
 barplot(
-  dat$m, names.arg=dat$n, horiz=T,
-  main="Bolas Reincidentes nos Concursos da Lotofácil",
+  frequencias, names.arg=possiveis, horiz=T, col=c('#33cc66', '#6666ff'),
+  main=paste("Bolas Reincidentes nos", 1+sum(frequencias), "Concursos da Lotofácil"),
   ylab="número de bolas reincidentes",
-  xlab="número de concursos",
-  col=c('#33cc66', '#6666ff'), xlim=c(0, upper), xaxt='n'
+  xlab="número de concursos", xlim=c(0, major), xaxt='n'
 )
-axis(side=1, seq(0, upper, 100))
-rug(side=1, seq(50, upper, 100), ticksize=-.0125, lwd=1)
+axis(side=1, seq(0, major, 100))
+rug(side=1, seq(50, major, 100), ticksize=-.0125, lwd=1)
 
-media <- sum(dat$n * dat$m) / sum(dat$m)  # média ponderada
+abline(v=seq(50, major, 50), lty="dotted", col='lightgray')
+
+media <- sum(possiveis * frequencias) / sum(frequencias)  # média ponderada
 
 text(
   sprintf("Média de %5.2f bolas reincidentes por concurso.", media),
   x=par("usr")[2]/2, y=par("usr")[4],
-  adj=c(.5, 1), cex=1.3, font=2, col='#ff6600'
+  adj=c(.5, 1), cex=1.35, font=2, col='#ff6600'
 )
 
 dev.off()
 
-system('display img/reincidencias.png')
-
+system(paste('display', fname))
