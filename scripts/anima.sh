cf259b931f0ba07846a909d02ba1009f53fc060f
dekassegui <dekassegui@gmail.com>
2020-06-07 12:34:35 -0300

    Ajuste complementar da animação.

--- scripts/anima.sh
+++ scripts/anima.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-. scripts/hack.sh
+# Montagem da animação via ffmpeg.
 
-# arquivo de áudio prefixo da animação
-audio=video/audio/intro.wav
-# quality ranges from 0 to 51 :: 0 is lossless, 23 is the default, 51 is the
-# worst quality possible and a sane range is 17–28
-quality=34
-# ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
-speed="medium"
-# film animation grain stillimage fastdecode zerolatency psnr
-tune="animation"
-# cria a animação composta de quadros e áudio
-ffmpeg -f concat -i video/roteiro.txt -i $audio -vf 'scale=800:600' \
-  -c:v libx264 -profile:v baseline -preset $speed -tune $tune -crf $quality \
-  -pix_fmt yuv420p -c:a aac -b:a 96k -y video/fun.mp4
+# checa pré-requisito
+if [[ ! $( which ffmpeg ) ]]; then
+
+  echo -e "\nErro: Pacote \"ffmpeg\" não está disponível.\n"
+
+else
+
+  . scripts/hack.sh   # agrega quadros de transição suave
+
+  # arquivo de áudio prefixo da animação
+  audio=video/audio/intro.wav
+  # quality ranges from 0 to 51 :: 0 is lossless, 23 is the default, 51 is the
+  # worst quality possible and a sane range is 17–28
+  quality=34
+  # ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
+  speed="medium"
+  # film animation grain stillimage fastdecode zerolatency psnr
+  tune="animation"
+  # cria a animação composta de quadros e áudio
+  ffmpeg -f concat -i video/roteiro.txt -i $audio -vf 'scale=800:600' \
+    -c:v libx264 -profile:v baseline -preset $speed -tune $tune -crf $quality \
+    -pix_fmt yuv420p -c:a aac -b:a 96k -y video/fun.mp4
+
+fi
--- scripts/hack.sh
+++ scripts/hack.sh
@@ -1,55 +1,60 @@
 #!/bin/bash
 
 # Montagem do arquivo do roteiro da animação melhorado com SFX "blend" para
-# transição do primeiro para o segundo quadro da animação.
+# transição suave do primeiro para o segundo quadro da animação.
 
 # checa pré-requisito
 if [[ ! $( which convert ) ]]; then
-  echo -e "\nErro: pacote \"imagemagick\" não está disponível\n"
-  exit 0
+
+  echo -e "\nNota: Pacote \"imagemagick\" não está disponível.\n"
+
+else
+
+  printf "\n> Criando quadros da transição"
+
+  rm -f video/quadros/kapa*   # exclui arquivos de transição antigos
+
+  exec 3< video/roteiro.txt 4> /tmp/enhanced.dat  # open files
+
+  # retorna a próxima linha do roteiro pertinente ao padrão regexp fornecido
+  READ(){
+    local LINE
+    while IFS= read -u 3 -r LINE; do [[ $LINE =~ $1 ]] && break; done
+    echo $LINE
+  }
+
+  # leitura e impressão da primeira declaração de arquivo – capa da transição –
+  # e respectiva duração – tempo de exposição do quadro –
+  line=$( READ "^file" )
+  echo -e $line"\n"$( READ '^duration' ) >&4
+  capa=$( echo ${line#* } | tr -d "'\"" )
+
+  # leitura da segunda declaração de arquivo – contracapa da transição –
+  # ignorando declarações de quadros de transição se roteiro já processado
+  line=$( READ "^file" )
+  [[ $line =~ ^file\ .*both- ]] || line=$( READ "^file\ .*both-" )
+  contra_capa=$( echo ${line#* } | tr -d "'\"" )
+
+  # loop de criação dos quadros da transição
+  for (( opacity=3; opacity<100; opacity=opacity+3 )); do
+    printf "."
+    printf -v fname "quadros/kapa-%02d.png" $opacity
+    # cria arquivo de quadro de transição
+    convert video/$capa video/$contra_capa -gravity center -antialias \
+      -alpha on -compose blend -define compose:args=$opacity \
+      -composite video/$fname
+    # insere info de quadro de transição no roteiro
+    echo -e "file '$fname'\nduration 0.125" >&4
+  done
+
+  # completa o roteiro com a segunda declaração – pendente – e restantes
+  while [[ -n $line ]]; do echo $line >&4; IFS= read -u 3 -r line; done
+
+  exec 3<&- 4>&-  # close files
+
+  # substituição do roteiro pré-existente pelo melhorado
+  mv /tmp/enhanced.dat video/roteiro.txt
+
+  printf "finalizado.\n\n"
+
 fi
-
-echo -e "\nProcessando quadros da animação, aguarde.\n"
-
-rm -f video/quadros/kapa*   # limpeza do buffer
-
-exec 3< video/roteiro.txt 4> /tmp/enhanced.dat    # open files
-
-# retorna linha do roteiro pertinente ao padrão regexp fornecido
-READ(){
-  local LINE
-  while IFS= read -u 3 -r LINE; do [[ $LINE =~ $1 ]] && break; done
-  echo $LINE
-}
-
-# retorna o path do arquivo na declaração fornecida
-GET(){ echo ${1#* } | tr -d "'\""; }
-
-# leitura da primeira declaração de arquivo – capa da transição
-line=$( READ "^file" )
-echo -e $line"\n"$( READ '^duration' ) >&4
-capa=$( GET "$line" )
-
-# leitura da segunda declaração de arquivo – contracapa da transição
-line=$( READ "^file" )
-[[ $line =~ ^file\ .*kapa- ]] && line=$( READ "^file\ .*both-" )
-contra_capa=$( GET "$line" )
-
-# loop de criação dos quadros da transição
-for (( opacity=3; opacity<100; opacity=opacity+3 )); do
-  printf -v fname "quadros/kapa-%02d.png" $opacity
-  # cria arquivo de quadro de transição
-  convert video/$capa video/$contra_capa \
-    -adaptive-blur 0x1 -gravity center -alpha on -compose blend \
-    -define compose:args=$opacity -composite video/$fname
-  # insere info de quadro de transição no roteiro
-  echo -e "file '$fname'\nduration 0.125" >&4
-done
-
-# completa o roteiro com a declaração pendente e restantes
-while [[ -n $line ]]; do echo $line >&4; IFS= read -u 3 -r line; done
-
-exec 3<&- 4>&-    # close files
-
-# substituição do roteiro original pelo melhorado
-mv /tmp/enhanced.dat video/roteiro.txt
