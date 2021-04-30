9f8bc0b61b3de44a057551991b876c10f43b788c
dekassegui <dekassegui@gmail.com>
2021-04-30 19:56:56 -0300

    Ajuste de desempenho no script de contagem.

--- sql/sequencias-02.sql
+++ sql/sequencias-02.sql
@@ -2,14 +2,14 @@
 .load "/usr/lib/sqlite3/pcre.so"
 
 -- contagem de concursos que contém sequências de números sorteados consecutivos
--- com comprimento 1 até 15, unicamente identificadas nas respectivas bitmasks
--- via expressões regulares que ignoram subsequências
-WITH RECURSIVE CTE AS (
-  SELECT "^1{%d}0|01{%d}0|01{%d}$" AS fmt
-), REX AS ( -- montagem das expressões regulares
-  SELECT 1 AS n, printf(fmt, 1, 1, 1) AS expression FROM CTE
+-- com comprimento 1 até 15, identificando primeiras ocorrências nas respectivas
+-- bitmasks via expressões regulares que ignoram subsequências
+WITH RECURSIVE CTE (fmt) AS (
+  SELECT "(?# to search the first occurrence)[^1]1{%d}[^1]"
+), REX (n, expression) AS ( -- montagem única das 15 expressões regulares
+  SELECT 1, printf(fmt, 1) FROM CTE
   UNION ALL
-  SELECT n+1, printf(fmt, n+1, n+1, n+1) FROM CTE, REX WHERE n < 15
-), LST AS ( -- identificação das sequências
-  SELECT n AS width FROM REX, bitmasks WHERE mask regexp expression
+  SELECT n+1, printf(fmt, n+1) FROM CTE, REX WHERE n < 15
+), LST (width) AS ( -- identificação das sequências
+  SELECT n FROM REX, bitmasks WHERE mask regexp expression
 ) SELECT width, count(1) AS times FROM LST GROUP BY width;
